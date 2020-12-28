31d5a3086f1b334918eb701087d83dac446f6b92
Taryn Martin <taryn.martin919@gmail.com>
2020-12-28 16:47:40 -0700

    Add folder structure for components

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Taryn Martin</h1>
+        <h2>Front End Software Engineer</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
