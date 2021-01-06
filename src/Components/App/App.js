fabb66915b356622f23cc76cdf541f891705428d
Taryn Martin <taryn.martin919@gmail.com>
2021-01-05 18:22:22 -0700

    Change icon package, add formatting for icons, and put styled components in separate file

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,130 +1,32 @@
 import './App.css';
-import styled from 'styled-components'
-import { FaLinkedin, FaGithub, FaGraduationCap, FaCode, FaBreadSlice, FaInfo } from "react-icons/fa";
-import { GrContact, GrBriefcase } from "react-icons/gr";
-import { IconContext} from 'react-icons';
-
-const StyledApp = styled.div`
-  background-color: teal;
-  color: white;
-  margin: 0;
-  padding: 0;
-  height: 100vh;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(5, 1fr);
-`
-
-const Button = styled.button`
-  background-color: none;
-  border: none;
-  font-size: 1em;
-`
-
-const AboutButton = styled(Button)`
-  grid-row: 3;
-  grid-column: 1;
-`;
-
-const ProjectsButton = styled(Button)`
-  grid-row: 1;
-  grid-column: 2;
-  text-align: center;
-`
-
-const EducationButton = styled(Button)`
-  grid-row: 2;
-  grid-column: 1;
-  text-align: right;
-`
-
-const ExperienceButton = styled(Button)`
-  grid-row: 2;
-  grid-column: 3;
-  text-align: left;
-`
-
-const HobbiesButton = styled(Button)`
-  grid-row: 3;
-  grid-column: 3;
-`
-
-const ContactButton = styled(Button)`
-  grid-row: 5;
-  grid-column: 2;
-`
-
-const LinkedinButton = styled(Button)`
-  grid-row: 4;
-  grid-column: 1;
-  text-align: right;
-`
-
-const GithubButton = styled(Button)`
-  grid-row: 4;
-  grid-column: 3;
-  text-align: left;
-`
-
-const JobInfo = styled.div`
-  grid-row: 3;
-  grid-column: 2;
-  text-align: center;
-`
-
-const StyledName = styled.h1`
-  color: white;
-  font-size: 2em;
-`
-
-const StyledJobTitle = styled.h2`
-  color: white;
-  font-size: 1em;
-`
+import {StyledApp, AboutButton, AboutLogo, ProjectsButton, ProjectLogo, EducationButton, EduLogo, ExperienceButton, ExpLogo, HobbiesButton, HobbyLogo, ContactButton, ContactLogo, LinkedinButton, LinkedinLogo, GithubButton, GithubLogo, JobInfo, StyledName, StyledJobTitle } from './Styles.js';
 
 function App() {
   return (
     <StyledApp>
       <AboutButton>
-        <IconContext.Provider value={{ size: "2em" }}>
-          <FaInfo />
-        </IconContext.Provider>
-        About
+        <AboutLogo />
       </AboutButton>
       <ExperienceButton>
-        <IconContext.Provider value={{ size: "2em" }}>
-          <GrBriefcase />
-        </IconContext.Provider>
-        Experience
+        <ExpLogo />
       </ExperienceButton>
       <EducationButton>
-        <IconContext.Provider value={{ size: "2em" }}>
-          {" "}
-          <FaGraduationCap />
-        </IconContext.Provider>
-        Education
+        <EduLogo />
       </EducationButton>
       <HobbiesButton>
-        <IconContext.Provider value={{ size: "2em" }}>
-          <FaBreadSlice />
-        </IconContext.Provider>
-        Hobbies
+        <HobbyLogo />
       </HobbiesButton>
       <LinkedinButton>
-        <IconContext.Provider value={{ size: "2em" }}><FaLinkedin /></IconContext.Provider>
-        LinkedIn
+        <LinkedinLogo />
       </LinkedinButton>
       <GithubButton>
-        <IconContext.Provider value={{ size: "2em" }}><FaGithub /></IconContext.Provider>
-        Github
+        <GithubLogo />
       </GithubButton>
       <ProjectsButton>
-        <IconContext.Provider value={{ size: "2em" }}><FaCode /></IconContext.Provider>
-        Projects
+        <ProjectLogo />
       </ProjectsButton>
       <ContactButton>
-        <IconContext.Provider value={{ size: "2em" }}><GrContact /></IconContext.Provider>
-        Contact
+        <ContactLogo />
       </ContactButton>
       <JobInfo>
         <StyledName>Taryn Martin</StyledName>
