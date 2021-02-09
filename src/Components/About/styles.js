5f0b5e968de69255d5ebc1513f6a03765a4cd943
Taryn Martin <taryn.martin919@gmail.com>
2021-02-09 15:54:01 -0700

    Move styles sheet from education to about

---
+++ src/Components/About/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyledContentParagraph = styled.p`
+  margin: 0 2em;
+  font-family: 'Rakkas', cursive;
+  font-size: 1.7em;
+  color: black;
+`;
+
+export const StyledBackground = styled.div`
+  background-color: white;
+  width: 85%;
+  height: 90%;
+  box-sizing: border-box;
+  margin: 3em;
+  padding: 1em;
+  overflow-y: scroll;
+  border-radius: 10px;
+`
