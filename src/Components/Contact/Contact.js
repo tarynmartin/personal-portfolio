2a5011b95979103854ef319ce1ae2ea514032cd1
Taryn Martin <taryn.martin919@gmail.com>
2021-02-18 16:18:34 -0700

    Add form for contact and basic styling

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import { StyledContentHeader, StyledBackground } from "./styles.js"
 
 function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
-    <>
-      <h1>Contact</h1>
-      <p>Pay meeeeeee</p>
-    </>
+    <StyledBackground>
+      <StyledContentHeader>Contact Me</StyledContentHeader>
+      <form>
+        <label htmlFor="name">Name:</label>
+        <input id="name" type="text" aria-label="Enter your name" placeholder="Joe Smith" onChange={(e) => setName(e.target.value)} required/>
+        <label htmlFor="email">Email Address:</label>
+        <input id="email" type="email" aria-label="Enter your email" placeholder="joe.smith@google.com" onChange={(e) => setEmail(e.target.value)} required/>
+        <label htmlFor="message">Message:</label>
+        <input id="message" type="text" aria-label="Why are you reaching out" placeholder="I'd love to work with you on a project!" onChange={(e) => setMessage(e.target.value)}/>
+        <input type="submit" value="submit" />
+      </form>
+    </StyledBackground>
   );
 }
 
---
+++ src/Components/Contact/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledBackground = styled.div`
+  grid-row: 1;
+  background-color: white;
+  width: 85%;
+  height: 90%;
+  box-sizing: border-box;
+  margin: 3em auto;
+  padding: 1em;
+  overflow-y: scroll;
+  border-radius: 10px;
+`
+
+export const StyledContentHeader = styled.h1`
+  font-family: "Rakkas", cursive;
+  font-size: 2.5em;
+  color: teal;
+  grid-column: 1 / span 2;
+  text-align: center;
+`
